dca7868f6764b4c0429e2d31edae571646fc233f
Kartik Jhamb <241230026@nitdelhi.ac.inm>
2025-07-22 02:33:40 +0530

    backend

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://jagdamba-store.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
